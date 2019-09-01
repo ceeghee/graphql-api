3d08b46d844af637305042bf4e3901c15535e9a2
Goodluck Ekene <goodluck.ekene@gmail.com>
2019-09-01 22:16:02 +0100

    made the type input to make use of enumerations allowing
    for just buy and sell possible inputs

--- src/schemas/calculate_price.ts
+++ src/schemas/calculate_price.ts
@@ -2,8 +2,14 @@ import { gql } from "apollo-server-express";
 
 export default gql`
   extend type Query {
-	  calculatePrice(type: String!, margin: Float!, exchangeRate: Float!): Float!
+	  calculatePrice(type: TypeInput!, margin: Float!, exchangeRate: Float!): Float!
   }
 
-
+  input TypeInput {
+	  buyorsel: types!
+  }
+  enum types{
+	  buy
+	  sell
+  }
 `;
